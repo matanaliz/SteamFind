c65fdbc4b75edf1c563b3f879c84463de248b15a
matanaliz <ol.kaspruk@gmail.com>
2016-02-22 16:55:12 +0200

    Resolved search path for steam api key file.
    File should be located  in stc module directory.

--- stc/__init__.py
+++ stc/__init__.py
@@ -1,15 +1,23 @@
 import os
 from steam import WebAPI
 
+import sys
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+keypath = os.path.join(basedir, 'steamapi.key')
+
 
 def get_key():
-    if os.path.isfile('steamapi.key'):
-        f = open('steamapi.key', 'r')
+    if os.path.isfile(keypath):
+        f = open(keypath, 'r')
         return str(f.readline())
-    return ''
+    return None
+
 
 steamKey = get_key()
-steamApp = WebAPI(key=steamKey)
+if not steamKey:
+    sys.exit('Steam API key was not provided.')
 
+steamApp = WebAPI(key=steamKey)
 
 from stc import steamcore
